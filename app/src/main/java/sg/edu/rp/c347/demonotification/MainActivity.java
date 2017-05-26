e9f6b8be70e17c752ad1295e5775fd1b0f4fa21e
15017199 <15017199@myrp.edu.sg>
2017-05-26 13:34:06 +0800

    2nd commit

--- app/src/main/java/sg/edu/rp/c347/demonotification/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/demonotification/MainActivity.java
@@ -1,18 +1,18 @@
 package sg.edu.rp.c347.demonotification;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
+        import android.app.Notification;
+        import android.app.NotificationManager;
+        import android.app.PendingIntent;
+        import android.media.RingtoneManager;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.Button;
+        import android.content.Intent;
 
-import sg.edu.rp.c347.demonotification.R;
+        import sg.edu.rp.c347.demonotification.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,19 +27,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnNotify1 = (Button) findViewById(R.id.btnNotify1);
+        btnNotify2 = (Button) findViewById(R.id.btnNotify2);
 
         btnNotify1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
 
                 PendingIntent pIntent = PendingIntent.getActivity
-                        ( MainActivity.this, requestCode, intent,
+                        (MainActivity.this, requestCode, intent,
                                 PendingIntent.FLAG_CANCEL_CURRENT);
 
                 // Build notification
-                Notification.Builder builder = new Notification.Builder(
-                        MainActivity.this);
+                Notification.Builder builder = new Notification.Builder(MainActivity.this);
                 builder.setContentTitle("Amazing Offer!");
                 builder.setContentText("Subject");
                 builder.setSmallIcon(R.mipmap.ic_launcher);
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 //                builder.setSound(uri);
 //                builder.setPriority(Notification.PRIORITY_HIGH);
 
-                Uri  uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
                 builder.setSound(uri);
 
                 Notification n = builder.build();
@@ -60,8 +60,38 @@ public class MainActivity extends AppCompatActivity {
                 // programmatically later on
                 notificationManager.notify(notificationID, n);
                 finish();
+
+            }
+
+        });
+        btnNotify2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pIntent = PendingIntent.getActivity(MainActivity.this, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                Notification.BigTextStyle bigText = new Notification.BigTextStyle();
+                bigText.bigText("This is one big text - A quick brown fox jumps over a lazy brown dog bjhmjb");
+                bigText.setBigContentTitle("Big Text - Long Content");
+                bigText.setSummaryText("Reflection Journal?");
+
+                //build notification
+                Notification.Builder builder = new Notification.Builder(MainActivity.this);
+                builder.setContentTitle("Amazing Offer!");
+                builder.setContentText("Subject");
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+                builder.setContentIntent(pIntent);
+                builder.setStyle(bigText);
+                builder.setAutoCancel(true);
+
+                Notification n = builder.build();
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                //this replaces the existing notification with the same ID
+                notificationManager.notify(notificationID, n);
+                finish();
             }
         });
     }
-}
 
+    ;
+}
